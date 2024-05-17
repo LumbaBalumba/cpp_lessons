c3c824e71006dfb8ab81b4995bb939067dbffdc1
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-05-17 10:20:23 +0300

    add 11/11_4.cpp

---
+++ 11/11_4.cpp
@@ -0,0 +1,35 @@
+#include <cpr/cpr.h>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+
+void
+process_site(const std::string &site, std::mutex *mutex_ptr, int *result)
+{
+    auto r = cpr::Get(cpr::Url{"http://" + site});
+    mutex_ptr->lock();
+    std::cout << r.text << std::endl;
+    mutex_ptr->unlock();
+    *result = 1;
+}
+
+int
+main(int argc, char *argv[])
+{
+    std::vector<std::string> sites = {"i3alumba.ru", "habr.com", "vk.com", "youtube.com", "yandex.ru"};
+    std::vector<std::thread> threads;
+    std::mutex mutex;
+    int result;
+    for (auto &site: sites) {
+        threads.emplace_back(process_site, site, &mutex, &result);
+    }
+
+    for (auto &thread: threads) {
+        thread.join();
+    }
+
+    return 0;
+}
